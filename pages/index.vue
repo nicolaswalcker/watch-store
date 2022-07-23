25d68ed8c5391598248d09ca56c2853999cceef1
Nicolas Walcker <walcker_@outlook.com>
2022-07-23 12:28:03 -0300

    chore: create basic structure of project

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <Tutorial />
+  <main class="my-8">
+    <div class="container mx-auto px-6">
+      <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
+      <span class="mt-3 text-sm text-gray-500">200+ Products</span>
+      <div
+        class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
+      >
+        <product-card />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
+import ProductCard from '@/components/ProductCard'
 export default {
-  name: 'IndexPage',
+  name: 'TheIndex',
+  components: { ProductCard },
 }
 </script>
