43e8965d404a23b946436007209461bd8b49f2d3
Nicolas Walcker <walcker_@outlook.com>
2022-10-13 00:02:27 -0300

    test: add default layout tests

---
+++ layouts/default.unit.spec.js
@@ -0,0 +1,33 @@
+import { mount } from '@vue/test-utils';
+import DefaultLayout from '~/layouts/default.vue';
+import Cart from '~/components/Cart.vue';
+import { CartManager } from '~/managers/CartManager';
+
+describe('Default Layout', () => {
+  const mountLayout = () => {
+    const wrapper = mount(DefaultLayout, {
+      mocks: {
+        $cart: new CartManager(),
+      },
+      stubs: {
+        Nuxt: true,
+      },
+    });
+
+    return { wrapper };
+  };
+  it('should mount Cart', () => {
+    const { wrapper } = mountLayout();
+    expect(wrapper.findComponent(Cart).exists()).toBe(true);
+  });
+
+  it('should toggle Cart visibility', async () => {
+    const { wrapper } = mountLayout();
+    const button = wrapper.find('[data-test-id="toggle-button"]');
+
+    await button.trigger('click');
+    expect(wrapper.vm.isCartOpen).toBe(true);
+    await button.trigger('click');
+    expect(wrapper.vm.isCartOpen).toBe(false);
+  });
+});
